e78cfaf2c09ce8f08dbfa64e9d56dcc68f5ec0ff
Rob Spanton <rspanton@zepler.net>
2012-10-29 20:25:32 +0000

    Add setup.py and associated gubbins

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup( name = "kokimarker",
+       version = "0.0.1",
+       py_modules = [ "kokimarker" ],
+       packages = ["kokimarker"],
+       maintainer = "Rob Spanton",
+       maintainer_email = "rspanton@zepler.net",
+       url = "https://www.studentrobotics.org/trac/wiki/libkoki",
+       )
