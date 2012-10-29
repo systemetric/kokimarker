ce40576494e363441c5e45bd614528a0bfe7cb61
Rob Spanton <rspanton@zepler.net>
2012-10-29 20:29:41 +0000

    Install koki-marker-gen into ${whatever}/bin

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup( name = "kokimarker",
        version = "0.0.1",
        py_modules = [ "kokimarker" ],
        packages = ["kokimarker"],
+       scripts = [ "koki-marker-gen" ],
+
        maintainer = "Rob Spanton",
        maintainer_email = "rspanton@zepler.net",
        url = "https://www.studentrobotics.org/trac/wiki/libkoki",
